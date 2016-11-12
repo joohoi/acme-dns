f20ad1118843f8dc75dcf8415e1d6d094f639d21
Joona Hoikkala <joona@kuori.org>
2016-11-12 14:54:41 +0200

    Fix reserved name

--- util.go
+++ util.go
@@ -5,7 +5,7 @@ import (
 	"unicode/utf8"
 )
 
-func NormalizeString(s string, len int) string {
+func NormalizeString(s string, length int) string {
 	var ret string
 	re, err := regexp.Compile("[^A-Za-z\\-0-9]+")
 	if err != nil {
@@ -13,8 +13,8 @@ func NormalizeString(s string, len int) string {
 		return ""
 	}
 	ret = re.ReplaceAllString(s, "")
-	if utf8.RuneCountInString(ret) > len {
-		ret = ret[0:len]
+	if utf8.RuneCountInString(ret) > length {
+		ret = ret[0:length]
 	}
 	return ret
 }
