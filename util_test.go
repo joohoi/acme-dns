6df03ecc7203556947097bca55a2df2831d032ec
Joona Hoikkala <joona@kuori.org>
2018-08-10 17:08:21 +0300

    Add tests

--- util_test.go
+++ util_test.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"os"
+	"syscall"
 	"testing"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func TestSetupLogging(t *testing.T) {
@@ -93,5 +95,35 @@ func TestGetIPListFromHeader(t *testing.T) {
 
 			}
 		}
+	}
+}
+
+func TestFileCheckPermissionDenied(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "acmedns")
+	if err != nil {
+		t.Error("Could not create temporary file")
+	}
+	defer os.Remove(tmpfile.Name())
+	syscall.Chmod(tmpfile.Name(), 0000)
+	if fileIsAccessible(tmpfile.Name()) {
+		t.Errorf("File should not be accessible")
+	}
+	syscall.Chmod(tmpfile.Name(), 0644)
+}
+
+func TestFileCheckNotExists(t *testing.T) {
+	if fileIsAccessible("/path/that/does/not/exist") {
+		t.Errorf("File should not be accessible")
+	}
+}
+
+func TestFileCheckOK(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "acmedns")
+	if err != nil {
+		t.Error("Could not create temporary file")
+	}
+	defer os.Remove(tmpfile.Name())
+	if !fileIsAccessible(tmpfile.Name()) {
+		t.Errorf("File should be accessible")
 	}
 }
