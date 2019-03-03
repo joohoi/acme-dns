67dc8a2cc35813d58d02df89b24d0a7d4031d017
tcely <tcely@users.noreply.github.com>
2019-03-03 15:04:38 -0500

    auth: using headers is optional
    The POST data now contains:
    
    - txt
    - subdomain
    - password
    
    This still checks `X-Api-Key` and `X-Api-User` for backwards compatibility. If a user, is provided we'll still check it, but it is now optional.

--- auth.go
+++ auth.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net"
 	"net/http"
 
@@ -19,31 +18,42 @@ const ACMETxtKey key = 0
 // Auth middleware for update request
 func Auth(update httprouter.Handle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		postData := ACMETxt{}
 		userOK := false
-		user, err := getUserFromRequest(r)
-		if err == nil {
-			if updateAllowedFromIP(r, user) {
-				dec := json.NewDecoder(r.Body)
-				err = dec.Decode(&postData)
-				if err != nil {
-					log.WithFields(log.Fields{"error": "json_error", "string": err.Error()}).Error("Decode error")
-				}
-				if user.Subdomain == postData.Subdomain {
-					userOK = true
-				} else {
-					log.WithFields(log.Fields{"error": "subdomain_mismatch", "name": postData.Subdomain, "expected": user.Subdomain}).Error("Subdomain mismatch")
-				}
-			} else {
-				log.WithFields(log.Fields{"error": "ip_unauthorized"}).Error("Update not allowed from IP")
-			}
+		userSupplied := false
+		user := ACMETxt{}
+		postData, err := decodePostData(r)
+		if err != nil {
+			log.WithFields(log.Fields{"error": "json_error", "string": err.Error()}).Error("Decode error")
 		} else {
-			log.WithFields(log.Fields{"error": err.Error()}).Error("Error while trying to get user")
+			userSupplied = getCredsFromRequest(r, &postData)
+		}
+		if !validSubdomain(postData.Subdomain) {
+			log.WithFields(log.Fields{"error": "invalid_subdomain"}).Error("Invalid subdomain UUID")
+		} else {
+			user, err = DB.GetBySubdomain(postData.Subdomain)
+			if err != nil {
+				log.WithFields(log.Fields{"error": "subdomain_not_found", "string": err.Error()}).Error("Subdomain is not registered")
+			} else {
+				if updateAllowedFromIP(r, user) {
+					if correctPassword(postData.Password, user.Password) {
+						if userSupplied {
+							userOK = postData.Username == user.Username
+						} else {
+							userOK = true
+						}
+					} else {
+						log.WithFields(log.Fields{"error": "invalid_password"}).Error("Password was not correct")
+					}
+				} else {
+					log.WithFields(log.Fields{"error": "ip_unauthorized"}).Error("Update not allowed from IP")
+				}
+			}
 		}
 		if userOK {
 			// Set user info to the decoded ACMETxt object
 			postData.Username = user.Username
 			postData.Password = user.Password
+			postData.Subdomain = user.Subdomain
 			// Set the ACMETxt struct to context to pull in from update function
 			ctx := context.WithValue(r.Context(), ACMETxtKey, postData)
 			update(w, r.WithContext(ctx), p)
@@ -55,28 +65,31 @@ func Auth(update httprouter.Handle) httprouter.Handle {
 	}
 }
 
-func getUserFromRequest(r *http.Request) (ACMETxt, error) {
-	uname := r.Header.Get("X-Api-User")
-	passwd := r.Header.Get("X-Api-Key")
-	username, err := getValidUsername(uname)
+func decodePostData(r *http.Request) (ACMETxt, error) {
+	postData := ACMETxt{}
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(&postData)
 	if err != nil {
-		return ACMETxt{}, fmt.Errorf("Invalid username: %s: %s", uname, err.Error())
+		return ACMETxt{}, err
 	}
-	if validKey(passwd) {
-		dbuser, err := DB.GetByUsername(username)
-		if err != nil {
-			log.WithFields(log.Fields{"error": err.Error()}).Error("Error while trying to get user")
-			// To protect against timed side channel (never gonna give you up)
-			correctPassword(passwd, "$2a$10$8JEFVNYYhLoBysjAxe2yBuXrkDojBQBkVpXEQgyQyjn43SvJ4vL36")
+	return postData, nil
+}
 
-			return ACMETxt{}, fmt.Errorf("Invalid username: %s", uname)
+func getCredsFromRequest(r *http.Request, postData *ACMETxt) bool {
+	if !validKey(postData.Password) {
+		key := r.Header.Get("X-Api-Key")
+		if validKey(key) {
+			postData.Password = key
 		}
-		if correctPassword(passwd, dbuser.Password) {
-			return dbuser, nil
-		}
-		return ACMETxt{}, fmt.Errorf("Invalid password for user %s", uname)
 	}
-	return ACMETxt{}, fmt.Errorf("Invalid key for user %s", uname)
+
+	user := r.Header.Get("X-Api-User")
+	uname, err := getValidUsername(user)
+	if err != nil {
+		return false
+	}
+	postData.Username = uname
+	return true
 }
 
 func updateAllowedFromIP(r *http.Request, user ACMETxt) bool {
