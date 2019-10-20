b6b306d5f19f53db7175cce0b3f8f9a50e5286e8
Joona Hoikkala <joona@kuori.org>
2019-10-20 22:30:26 +0300

    Fix test and add docstrings

--- challengeprovider.go
+++ challengeprovider.go
@@ -2,14 +2,17 @@ package main
 
 import "github.com/go-acme/lego/challenge/dns01"
 
+// ChallengeProvider implements go-acme/lego Provider interface which is used for ACME DNS challenge handling
 type ChallengeProvider struct {
 	servers []*DNSServer
 }
 
+// NewChallengeProvider creates a new instance of ChallengeProvider
 func NewChallengeProvider(servers []*DNSServer) ChallengeProvider {
 	return ChallengeProvider{servers: servers}
 }
 
+// Present is used for making the ACME DNS challenge token available for DNS
 func (c *ChallengeProvider) Present(_, _, keyAuth string) error {
 	_, token := dns01.GetRecord("whatever", keyAuth)
 	for _, s := range c.servers {
@@ -18,6 +21,7 @@ func (c *ChallengeProvider) Present(_, _, keyAuth string) error {
 	return nil
 }
 
+// CleanUp is called after the run to remove the ACME DNS challenge tokens from DNS records
 func (c *ChallengeProvider) CleanUp(_, _, _ string) error {
 	for _, s := range c.servers {
 		s.PersonalKeyAuth = ""
